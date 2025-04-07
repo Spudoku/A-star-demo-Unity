acfc6048329627d7b11fd9f57003f4b7ad49906c
Spudoku <calebisntshort@gmail.com>
2025-04-07 11:43:00 -0700

    Began waypoints and graphs
    ~90 minutes

--- Assets/Scripts/FollowWaypoint.cs
+++ Assets/Scripts/FollowWaypoint.cs
@@ -1,16 +1,62 @@
+using System.Linq;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class FollowWaypoint : MonoBehaviour
 {
+
+    public GameObject[] waypoints;
+
+    GameObject tracker;
+    int currentWayPoint = 0;
+
+    public float rotSpeed = 30f;
+    public float speed = 10.0f;
+
+    public float lookAhead = 10f;
+
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        tracker = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+        DestroyImmediate(tracker.GetComponent<Collider>());
+        tracker.GetComponent<MeshRenderer>().enabled = false;
+        tracker.transform.SetPositionAndRotation(transform.position, transform.rotation);
     }
 
-    // Update is called once per frame
+    void ProgressTracker()
+    {
+        if (Vector3.Distance(tracker.transform.position, transform.position) >= lookAhead) return;
+
+        Vector3 currentWPPos = waypoints[currentWayPoint].transform.position;
+        if (Vector3.Distance(tracker.transform.position, currentWPPos) < 3.0f)
+        {
+            currentWayPoint++;
+            if (currentWayPoint >= waypoints.Length)
+            {
+                currentWayPoint = 0;
+            }
+        }
+        tracker.transform.LookAt(currentWPPos);
+        tracker.transform.Translate(0, 0, speed * 5.1f * Time.deltaTime);
+    }
+
     void Update()
     {
-        
+        Vector3 currentWPPos = waypoints[currentWayPoint].transform.position;
+
+        ProgressTracker();
+
+        //transform.LookAt(waypoints[currentWayPoint].transform.position);
+
+        // SLERP (so thirsty)
+        Quaternion lookatWP = quaternion.LookRotation(tracker.transform.position - transform.position, Vector3.up);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookatWP, Time.deltaTime * rotSpeed);
+        transform.Translate(0, 0, speed * Time.deltaTime);
+
+
     }
 }
---
+++ Assets/Scripts/Graphs/Edge.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Edge
+{
+    public Node startNode;
+    public Node endNode;
+
+
+    public Edge(Node from, Node to)
+    {
+        startNode = from;
+        endNode = to;
+    }
+
+
+}
